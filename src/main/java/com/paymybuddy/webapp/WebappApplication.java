40c48c253e13eee0c752a5e021baa6d3812b2fdd
etienne-verraest <verraest.etienne@gmail.com>
2022-08-09 12:11:02 +0200

    Disabled default form login and created a SignUp form

--- src/main/java/com/paymybuddy/webapp/WebappApplication.java
+++ src/main/java/com/paymybuddy/webapp/WebappApplication.java
@@ -19,7 +19,7 @@ public class WebappApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		//
 	}
 
 }
--- src/main/java/com/paymybuddy/webapp/controller/UserController.java
+++ src/main/java/com/paymybuddy/webapp/controller/UserController.java
@@ -1,8 +1,22 @@
 package com.paymybuddy.webapp.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 
+	@GetMapping("/signup")
+	public ModelAndView showSignUpForm() {
+
+		String viewName = "SignUpForm";
+		Map<String, Object> model = new HashMap<String, Object>();
+
+		return new ModelAndView(viewName, model);
+
+	}
 }

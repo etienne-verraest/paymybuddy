26432f4e5eae3426d53821ac6e5f0fb27dac3991
etienne-verraest <verraest.etienne@gmail.com>
2022-08-18 16:30:41 +0200

    Added bank form

---
+++ src/main/java/com/paymybuddy/webapp/controller/BankAccountController.java
@@ -0,0 +1,59 @@
+package com.paymybuddy.webapp.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.paymybuddy.webapp.config.constants.ViewNameConstants;
+import com.paymybuddy.webapp.model.User;
+import com.paymybuddy.webapp.model.dto.BankAccountAddDto;
+import com.paymybuddy.webapp.service.UserService;
+
+@Controller
+public class BankAccountController {
+
+	@Autowired
+	private UserService userService;
+
+	private static String viewName = ViewNameConstants.BANK_VIEW_NAME;
+
+	@GetMapping("/bank")
+	public ModelAndView showBankPage() {
+
+		// Get current logged user
+		String mail = userService.getEmailOfLoggedUser();
+		User user = userService.findUserByMail(mail);
+
+		Map<String, Object> model = new HashMap<>();
+		model.put("bankAccountAddDto", new BankAccountAddDto());
+		// TODO : Put values and populate fields
+
+		return new ModelAndView(viewName, model);
+	}
+
+	@PostMapping("/bank")
+	public ModelAndView submitBankForm(@Valid BankAccountAddDto bankAccountAddDto, BindingResult bindingResult) {
+
+		// If there are errors when adding the connection
+		if (bindingResult.hasErrors()) {
+			return new ModelAndView(viewName);
+		}
+
+		// Get current logged user
+		String mail = userService.getEmailOfLoggedUser();
+		User user = userService.findUserByMail(mail);
+
+		// Get buddies of logged user
+		Map<String, Object> model = new HashMap<>();
+		return new ModelAndView(viewName, model);
+	}
+
+}
---
+++ src/main/java/com/paymybuddy/webapp/model/dto/BankAccountAddDto.java
@@ -0,0 +1,23 @@
+package com.paymybuddy.webapp.model.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BankAccountAddDto {
+
+	@NotEmpty(message = "Bank name must not be empty")
+	private String bankName;
+
+	@Size(min = 23, max = 23, message = "Your RIB should have 23 characters only")
+	private String rib;
+
+	@Size(min = 14, max = 34, message = "Your IBAN should have at least 14 characters and a maximum of 34 characters")
+	private String iban;
+}

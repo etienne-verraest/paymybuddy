92ea684050eb3b2508306cb8bea85e87d421b262
etienne-verraest <verraest.etienne@gmail.com>
2022-08-24 14:06:07 +0200

    Implemented methods in service for transactions

--- src/main/java/com/paymybuddy/webapp/model/dto/TransactionDto.java
+++ src/main/java/com/paymybuddy/webapp/model/dto/TransactionDto.java
@@ -2,6 +2,8 @@ package com.paymybuddy.webapp.model.dto;
 
 import java.time.Instant;
 
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,7 @@ public class TransactionDto {
 
 	private Integer recipientId;
 
+	@Size(min = 1, max = 150, message = "Please set a description (max 150 characters)")
 	private String description;
 
 	private Double amount;
--- src/main/java/com/paymybuddy/webapp/repository/TransactionRepository.java
+++ src/main/java/com/paymybuddy/webapp/repository/TransactionRepository.java
@@ -1,11 +1,26 @@
 package com.paymybuddy.webapp.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.paymybuddy.webapp.model.Transaction;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE user u SET u.balance = (u.balance - :amount) WHERE u.id = :senderId", nativeQuery = true)
+	void updateSenderBalance(@Param("senderId") Integer senderId, @Param("amount") Double amountWithFee);
+
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE user u SET u.balance = (u.balance + :amount) WHERE u.id = :recipientId", nativeQuery = true)
+	void updaterRecipientBalance(@Param("recipientId") Integer recipientId, @Param("amount") Double amountWithoutFee);
 
 }
